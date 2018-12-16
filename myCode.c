6b70b45b668ecf41bd6d0b63660dd1605d5781b4
es5es5 <es5e5s@nate.com>
2018-12-16 15:06:58 +0900

    버튼 누르면 해당하는 LED가 점멸된다.

--- myCode.c
+++ myCode.c
@@ -1,44 +1,42 @@
-#define PLACE-A 2 // 초기
-#define PLACE-B 3 // 중기
-#define PLACE-C 4 // 후기
+#define PLACE_FORMER 2 // 초기
+#define PLACE_MIDDLE 3 // 중기
+#define PLACE_LATTER 4 // 후기
 
 // RFID 를 대체할 버튼
-#define BUTTON-A 5
-#define BUTTON-B 6
-#define BUTTON-C 7
+#define BUTTON_FORMER 5
+#define BUTTON_MIDDLE 6
+#define BUTTON_LATTER 7
 
-#define LED-A 8 // LED
-#define LED-B 9
-#define LED-C 10
-
-boolean buttonA = false;
-boolean buttonB = false;
-boolean buttonC = false;
+#define LED_FORMER 8 // LED
+#define LED_MIDDLE 9
+#define LED_LATTER 10
 
 void setup() {
-  pinMode(LED-A, OUTPUT);
-  pinMode(LED-A, OUTPUT);
-  pinMode(LED-A, OUTPUT);
-  pinMode(BUTTON-A, INPUT);
-  pinMode(BUTTON-B, INPUT);
-  pinMode(BUTTON-C, INPUT);
-  pinMode(LED-A, OUTPUT);
-  pinMode(LED-B, OUTPUT);
-  pinMode(LED-C, OUTPUT);
+  pinMode(PLACE_FORMER, OUTPUT);
+  pinMode(PLACE_MIDDLE, OUTPUT);
+  pinMode(PLACE_LATTER, OUTPUT);
+  pinMode(BUTTON_FORMER, INPUT);
+  pinMode(BUTTON_MIDDLE, INPUT);
+  pinMode(BUTTON_LATTER, INPUT);
+  pinMode(LED_FORMER, OUTPUT);
+  pinMode(LED_MIDDLE, OUTPUT);
+  pinMode(LED_LATTER, OUTPUT);
 }
 
+
 void loop() {
-  if (digitalRead(BUTTON-A) == HIGH) {
-    digitalWrite(LED-A, HIGH);
+  // 버튼 누르면 해당하는 불이 들어온다.
+  if (digitalRead(BUTTON_FORMER) == HIGH) {
+    digitalWrite(LED_FORMER, HIGH);
     delay(500);
-    digitalWrite(LED-A, LOW);
-  } else if (digitalRead(BUTTON-B) == HIGH){
-    digitalWrite(LED-B, HIGH);
+    digitalWrite(LED_FORMER, LOW);
+  } else if (digitalRead(BUTTON_MIDDLE) == HIGH){
+    digitalWrite(LED_MIDDLE, HIGH);
     delay(500);
-    digitalWrite(LED-B, LOW);
-  } else if (digitalRead(BUTTON-C) == HIGH) {
-    digitalWrite(LED-C, HIGH);
+    digitalWrite(LED_MIDDLE, LOW);
+  } else if (digitalRead(BUTTON_LATTER) == HIGH) {
+    digitalWrite(LED_LATTER, HIGH);
     delay(500);
-    digitalWrite(LED-C, LOW);
+    digitalWrite(LED_LATTER, LOW);
   }
 }
